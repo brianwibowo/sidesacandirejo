8b06304c67a39cad5ba21127610577a4286e3679
qiromm <mujahidilqirom@students.unnes.ac.id>
2026-04-12 18:31:18 +0700

    ceksession update

--- booking/login/ceksession.php
+++ booking/login/ceksession.php
@@ -1,10 +1,15 @@
 <?php
-if (isset($_SESSION['r3su'])){
-	if($_SESSION['r3su'] == 'bgn'){
-		header('location:bagian/');
-	}
-	else if($_SESSION['r3su'] == 'dmn'){
-		// header('location:admin/');
-	}
+// Kalau belum login sama sekali, tendang ke landing page
+if (!isset($_SESSION['r3su'])) {
+    header('Location: ../index.php');
+    exit();
+}
+
+// Kalau sudah login, arahkan ke halaman yang sesuai rolenya
+if ($_SESSION['r3su'] == 'bgn') {
+    header('location:bagian/');
+    exit();
+} else if ($_SESSION['r3su'] == 'dmn') {
+    // sudah di halaman yang benar, lanjut
 }
 ?>
